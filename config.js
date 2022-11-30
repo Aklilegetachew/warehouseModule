4855245bbbf2f041907afbbcb2251e73993b6984
Aklilegetachew <aklilegetachew@gmail.com>
2022-11-30 15:17:04 +0300

    deployment 2

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ dotenv.config({
 module.exports = {
   NODE_ENV: process.env.NODE_ENV || "development",
   HOST: process.env.HOST || "localhost",
-  PORT: process.env.PORT || 3001,
+  PORT: process.env.PORT || 3000,
   DBNAME:process.env.DATABASENAME,
   DBUSER: process.env.USER, 
   DBPASSWORD: process.env.DBPASSWORD
